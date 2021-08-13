4dab4745bc98d208b7f2be440019d3babc8dad8c
Joseph Son <sms0330@gmail.com>
2021-08-13 09:35:42 -0700

    Added Handle 404 not Found to HTTP API

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -10,6 +10,19 @@ class Api::ApplicationController < ApplicationController
     # needed for public HTTP apis, so we'll skip it.
     skip_before_action :verify_authenticity_token
 
+    #Error message handling
+    #To send a json error message when a user types in, for example: localhost:3000/api/v1/wrongthing
+    def not_found
+        render(
+            json: {
+                errors: [{
+                    type: "Not Found"
+                }]
+            },
+            status: :not_found #alias for 404 in rails
+        )
+    end
+
     private
 
     def authenticate_user!
--- config/routes.rb
+++ config/routes.rb
@@ -104,9 +104,17 @@ Rails.application.routes.draw do
       resource :session, only: [:create, :destroy]
       resources :users, only: [:create] do
         # get :current -> /api/v1/users/:user_id/current
-        get :current, on: :collection # => api/v1/users/current
+        get :current, on: :collection # -> api/v1/users/current
       end
     end
+    match "*unmatched_route", to: "application#not_found", via: :all
+    #The route will match with any URL that hasn't been matched already
+    #inside of the api namespace
+    #The "*" prefix in the routes path allows this route wildcard
+    #match ANYTHING
+    #The "via:" argument is required and is used to specify which methods this route applies to
+    #Example: via: [:get, :post, :patch]
+    #via: all will match all possible methods
   end
 
 end
